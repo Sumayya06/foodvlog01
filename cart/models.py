6b21078e59db6fc4ff7b1ffd9ac9025743c1e4c2
Sumayya06 <sumayyasalam06@gmail.com>
2022-05-10 20:52:40 +0530

    first commit

--- cart/models.py
+++ cart/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 class cartlist(models.Model):
     cart_id=models.CharField(max_length=250,unique=True)
     date_added=models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User,on_delete=CASCADE,null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
    
     def __str__(self):
         return self.cart_id
